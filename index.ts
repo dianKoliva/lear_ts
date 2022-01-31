fa48b4ee0cb729ee8ab4014061c687238dd801dd
dianKoliva <dianpretty01@gmail.com>
2022-01-31 21:42:43 +0200

    new changes

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
-console.log('Hello world');
+// console.log('Hello world');
+
+
+
+
+
+
+
+
+
+
 
 
 // a variable can not be assigned 2 values of a diff type
@@ -13,3 +23,16 @@ console.log('Hello world');
 // let dogAge = realAge * 7;
 
 // console.log(`${dogAge} years`);
+
+// let firstName = 'muriel!';
+
+// console.log(firstName.toUpperCase());
+
+// console.log(firstName.length);
+
+// Any
+// There are some places where TypeScript will not try to infer what type something is—generally when a variable is declared without being assigned an initial value. In situations where it isn’t able to infer a type, TypeScript will consider a variable to be of type any.
+
+// Variables of type any can be assigned to any value and TypeScript won’t give an error if they’re reassigned to a different type later on.
+
+
