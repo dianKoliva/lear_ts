30a3953549ffffe0ee08d45c24bb96ee7528d07d
dianKoliva <dianpretty01@gmail.com>
2022-01-31 21:35:37 +0200

    new changes

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
 console.log('Hello world');
+
+
+// a variable can not be assigned 2 values of a diff type
+
+// let aged = true;
+// let realAge = 0;
+
+// if (aged) {
+//   realAge = 4;
+// }
+
+// let dogAge = realAge * 7;
+
+// console.log(`${dogAge} years`);
