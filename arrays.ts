8352942898e31c737970a3ef9afcf6f132a1abb9
dianKoliva <dianpretty01@gmail.com>
2022-02-04 11:16:19 +0200

    new changes

--- arrays.ts
+++ arrays.ts
@@ -1,3 +1,9 @@
 let names: string[] = ['Danny', 'Samantha'];
 
 let smp: Array<string> = ['Danny', 'Samantha'];
+
+//destructing an array
+// var a =[10,"hello"] 
+// var [b,c] = a 
+// console.log( b )    
+// console.log( c ) 
